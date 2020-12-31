77f0eb89ec6bd32866b9f6eba637759cafa80a01
sanyukta-mittal <73833055+sanyukta-mittal@users.noreply.github.com>
2020-12-31 18:45:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var car, wall;
+var speed, weight;
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+
+  speed = random(55,90);
+  weight = random(400,1500);
+
+  car=createSprite(50,200,50,50)
+ 
+  wall=createSprite(1500,200,60,height/2)
+  wall.shapeColor(80,80,80)
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
